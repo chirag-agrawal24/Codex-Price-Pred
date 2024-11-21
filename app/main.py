588e9778bc33fc285f57fa5dba06bc59e0cb745c
Chirag Agrawal <2411chirag@gmail.com>
2024-11-21 11:16:40 +0530

    streamlit app

---
+++ app/main.py
@@ -0,0 +1,75 @@
+# codebasics ML course: codebasics.io, all rights reserverd
+import json
+import streamlit as st
+from price_calculator import calculate
+
+# Define the page layout
+st.title('Health Insurance Cost Predictor')
+
+with open("artifacts/column_datas.json") as json_file:
+    categorical_options = json.load(json_file)
+
+
+
+
+# We would mimic the structure of actual data given(before feature engineered)
+# Create a 4x4 grid layout
+row1 = st.columns(4)
+row2 = st.columns(4)
+row3 = st.columns(4)
+row4 = st.columns(4)
+
+# Assign inputs to the grid
+input_dict = {}
+
+# First row
+with row1[0]:
+    input_dict['age'] = st.number_input('Age', min_value=18, step=1, max_value=100)
+with row1[1]:
+    input_dict['gender'] = st.selectbox('Gender', categorical_options['gender'])
+with row1[2]:
+    input_dict['zone'] = st.selectbox('Geographical Zone', categorical_options['zone'])
+with row1[3]:
+    input_dict['occupation'] = st.selectbox('Occupation', categorical_options['occupation'])
+
+# Second row
+with row2[0]:
+    input_dict['income_levels'] = st.selectbox('Income Range (In Lakhs)', categorical_options['income_levels'])
+with row2[1]:
+    input_dict['current_brand'] = st.selectbox('Current Brand', categorical_options['current_brand'])
+with row2[2]:
+    input_dict['awareness_of_other_brands'] = st.selectbox('Awareness of Other Brands', categorical_options['awareness_of_other_brands'])
+with row2[3]:
+    input_dict['reasons_for_choosing_brands'] = st.selectbox('Choose Codex for', categorical_options['reasons_for_choosing_brands'])
+
+# Third row
+with row3[0]:
+    input_dict['consume_frequency(weekly)'] = st.selectbox('Consume Frequency (Weekly)', categorical_options['consume_frequency(weekly)'])
+with row3[1]:
+    input_dict['typical_consumption_situations'] = st.selectbox('Typical Consumption Situations', categorical_options['typical_consumption_situations'])
+with row3[2]:
+    input_dict['preferable_consumption_size'] = st.selectbox('Size Preference', categorical_options['preferable_consumption_size'])
+with row3[3]:
+    input_dict['flavor_preference'] = st.selectbox('Flavor Preference', categorical_options['flavor_preference'])
+    
+# Fourth row
+with row4[0]:
+    input_dict['health_concerns'] = st.selectbox('Health Concerns', categorical_options['health_concerns'])
+with row4[1]:
+    input_dict['packaging_preference'] = st.selectbox('Packaging Preference', categorical_options['packaging_preference'])
+with row4[2]:
+    input_dict['purchase_channel'] = st.selectbox('Purchase Channel', categorical_options['purchase_channel'])
+
+
+# Button to make prediction
+if st.button('Calculate Price Range'):
+
+    #logical erro 
+    if input_dict['age']>55 and input_dict['occupation']=="Student":
+        st.error("A Student can not be older than 55 Years")
+    elif input_dict['age']<40 and input_dict['occupation']=="Retired":
+        st.error("Your age does not seems to be Retirement age")
+
+    else:
+        price = calculate(input_dict)
+        st.success(f'Price Range: {price} INR')
---
+++ app/price_calculator.py
@@ -0,0 +1,87 @@
+import pandas as pd
+import json
+import joblib
+
+with open("artifacts/column_datas.json") as json_file:
+    columns_for_model = json.load(json_file)['columns']
+
+model_path= "artifacts/LightGBM_best_model.joblib"
+model=joblib.load(model_path)
+
+encoder_path="artifacts/target_encoder.joblib"
+target_encoder=joblib.load(encoder_path)
+
+
+def feature_engineered(input_dict:dict):
+    df = pd.DataFrame(input_dict,index=[0])
+
+
+    # Age Group
+
+    # why we write 17??? because in cut(right=True) means right value is included but not left 
+    # so if we write 18 then it will not be included in 18-25
+    age_bins = [17, 25, 35, 45, 55, 70, float('inf')]  # Define bins
+    age_labels = ['18-25', '26-35', '36-45', '46-55', '56-70', '70+']  # Define labels
+    df['age_group'] = pd.cut(df['age'], bins=age_bins, labels=age_labels, right=True)
+
+    #cf_ab_score
+    consume_frequency_mapping = {
+    "0-2 times": 1,
+    "3-4 times": 2,
+    "5-7 times": 3
+}
+
+    awareness_mapping = {
+    "0 to 1": 1,
+    "2 to 4": 2,
+    "above 4": 3
+}
+
+    df['frequency_score'] = df['consume_frequency(weekly)'].map(consume_frequency_mapping)
+    df['awareness_score'] = df['awareness_of_other_brands'].map(awareness_mapping)
+
+    df['cf_ab_score'] = df['frequency_score'] / (df['awareness_score'] + df['frequency_score'])
+
+    df['cf_ab_score'] = df['cf_ab_score'].round(2)
+
+    #zas score
+
+    zone_mapping = {
+    "Urban": 3,
+    "Metro": 4,
+    "Rural": 1,
+    "Semi-Urban": 2
+    }
+
+    income_mapping = {
+        "<10L": 1,
+        "10L - 15L": 2,
+        "16L - 25L": 3,
+        "26L - 35L": 4,
+        "> 35L": 5,
+        "Not Reported": 0
+    }
+
+    df['zone_score'] = df['zone'].map(zone_mapping)
+    df['income_score'] = df['income_levels'].map(income_mapping)
+    df['zas_score'] = df['zone_score'] * df['income_score']
+
+    condition1 = df['current_brand'] != "Established"  # Check if current_brand is not "Established"
+    condition2 = df['reasons_for_choosing_brands'].isin(['Price', 'Quality'])  # Check if reasons are "Price" or "Quality"
+
+
+    df['bsi'] = (condition1 & condition2).astype(int)  # Assign 1 if both conditions are true, else 0
+
+
+
+    output_df=df[columns_for_model]
+    return output_df
+
+def calculate(input_dict:dict):
+    
+    df=feature_engineered(input_dict)
+    print(df)
+    encoded_result = model.predict(df)
+    result=target_encoder.inverse_transform(encoded_result.reshape(-1, 1))[0,0]
+
+    return result
